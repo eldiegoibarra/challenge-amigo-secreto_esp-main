1293538f706664dce82d8cd4a140935b0ebdd9b0
eldiegoibarra <infodiegoibarra@gmail.com>
2025-03-07 11:48:48 +0000

    Testing git

---
+++ app.js
@@ -0,0 +1,16 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+function agregarAmigo(){
+    let nombre = document.getElementById("amigo").value.trim(); // usando DOM para extraer la informacion desde HTML
+    console.log(nombre); // revisando si los nombres se guardan en la consola
+
+    if (nombre === ""){
+        alert("Por favor ingresa un nombre válido."); // Alerta si no hay nombre ingresadd
+        return;
+    }
+
+}
+
+
+function sorteo(){
+
+}
