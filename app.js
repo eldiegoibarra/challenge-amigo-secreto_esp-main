9f29f004b66dd1cb6fa5934504015c32e21f4cd5
eldiegoibarra <infodiegoibarra@gmail.com>
2025-03-09 11:53:53 +0000

    limpiando, arreglando el codigo

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-function agregarAmigo(){
-    let nombre = document.getElementById("amigo").value.trim(); // usando DOM para extraer la informacion desde HTML
-    console.log(nombre); // revisando si los nombres se guardan en la consola
 
-    if (nombre === ""){
-        alert("Por favor ingresa un nombre válido."); // Alerta si no hay nombre ingresado
+let nombres = []; // Guardar nombres
+
+// Function para agregar amigos
+function agregarAmigo() {
+    let input = document.getElementById("amigo"); // Usando el DOM
+    let nombre = input.value;
+
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre válido."); // Haciendo click sin haber colocado un nombre
         return;
     }
 
-    let losNombres = JSON.parse(localStorage.getItem("losNombres")) || []; // Para los nombres agregados
-    losNombres.push(nombre); // agregar nuevo nombre
-    localStorage.setItem("losNombres", JSON.stringify(losNombres)); // Guarda y actualiza la lista 
-     document.getElementById("amigo").value = ""; // limpia la caja
-    listaAmigos(); // Update the list on screen
-   // alert("Nombre guardado") -  opcional
-
+    nombres.push(nombre);
+    input.value = ""; // Limpiar el input
+    mostrarLista();
 }
 
-function listaAmigos() {
-    let losNombres = JSON.parse(localStorage.getItem("losNombres"))
+// Function para elegir un nombre al azar
+function sortearAmigo() {
+    if (nombres.length === 0) {
+        alert("No hay nombres para sortear."); // Si no hay nombres y el usuario hace click en el boton sorteo...
+        return;
+    }
+    let index = Math.floor(Math.random() * nombres.length);
+    document.getElementById("resultado").textContent = "El amigos secreto es: " + nombres[index];
 }
+
+// Function para mostrar la lista
+function mostrarLista() {
+    let lista = document.getElementById("resultado");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < nombres.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = nombres[i];
+        lista.appendChild(li);
+    }
+}
+
+// Resetear cuando la pagina recarga
+window.onload = function () {
+    nombres = [];
+    document.getElementById("resultado").innerHTML = "";
+};
+
